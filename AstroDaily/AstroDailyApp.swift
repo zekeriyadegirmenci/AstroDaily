0ddcc7d56a95ab3c51f812818644038557737549
zekeriyadegirmenci <zekeriyade0@gmail.com>
2025-03-04 14:08:16 +0300

    Created project

---
+++ AstroDaily/AstroDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroDailyApp.swift
+//  AstroDaily
+//
+//  Created by Zekeriya Değirmenci on 4.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct AstroDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AstroDaily/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AstroDaily
+//
+//  Created by Zekeriya Değirmenci on 4.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
